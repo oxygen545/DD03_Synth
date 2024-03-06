3210e68b37366b4923ec9ebf3eefb6f89550f7c2
oxygen545 <damon@hopkins-family.org>
2024-03-06 09:00:15 -0600

    Added Wavetables

---
+++ include/AKWFTables.h
@@ -0,0 +1,50 @@
+#include <AKWF_saw8bit_512.h>
+#include <AKWF_sin8bit_512.h>
+#include <AKWF_squ8bit_512.h>
+#include <AKWF_tri8bit_512.h>
+#include <AKWF_piano_0016_512.h>
+#include <AKWF_eorgan_0003_512.h>
+#include <AKWF_sinharm_0001_512.h>
+#include <AKWF_sinharm_0002_512.h>
+#include <AKWF_sinharm_0003_512.h>
+#include <AKWF_sinharm_0004_512.h>
+#include <AKWF_sinharm_0005_512.h>
+#include <AKWF_sinharm_0006_512.h>
+#include <AKWF_sinharm_0007_512.h>
+#include <AKWF_sinharm_0008_512.h>
+#include <AKWF_sinharm_0009_512.h>
+#include <AKWF_sinharm_0010_512.h>
+#include <AKWF_sinharm_0011_512.h>
+#include <AKWF_sinharm_0012_512.h>
+#include <AKWF_sinharm_0013_512.h>
+#include <AKWF_sinharm_0014_512.h>
+#include <AKWF_sinharm_0015_512.h>
+#include <AKWF_sinharm_0016_512.h>
+
+
+
+#define NUM_CELLS 512
+#define NUM_WAVETABLES 22
+
+#define WAVE_DATA0 AKWF_sin8bit_512_DATA
+#define WAVE_DATA1 AKWF_tri8bit_512_DATA
+#define WAVE_DATA2 AKWF_saw8bit_512_DATA
+#define WAVE_DATA3 AKWF_squ8bit_512_DATA
+#define WAVE_DATA4 AKWF_piano_0016_512_DATA
+#define WAVE_DATA5 AKWF_eorgan_0003_512_DATA
+#define WAVE_DATA6 AKWF_sinharm_0001_512_DATA
+#define WAVE_DATA7 AKWF_sinharm_0002_512_DATA
+#define WAVE_DATA8 AKWF_sinharm_0003_512_DATA
+#define WAVE_DATA9 AKWF_sinharm_0004_512_DATA
+#define WAVE_DATA10 AKWF_sinharm_0005_512_DATA
+#define WAVE_DATA11 AKWF_sinharm_0006_512_DATA
+#define WAVE_DATA12 AKWF_sinharm_0007_512_DATA
+#define WAVE_DATA13 AKWF_sinharm_0008_512_DATA
+#define WAVE_DATA14 AKWF_sinharm_0009_512_DATA
+#define WAVE_DATA15 AKWF_sinharm_0010_512_DATA
+#define WAVE_DATA16 AKWF_sinharm_0011_512_DATA
+#define WAVE_DATA17 AKWF_sinharm_0012_512_DATA
+#define WAVE_DATA18 AKWF_sinharm_0013_512_DATA
+#define WAVE_DATA19 AKWF_sinharm_0014_512_DATA
+#define WAVE_DATA20 AKWF_sinharm_0015_512_DATA
+#define WAVE_DATA21 AKWF_sinharm_0016_512_DATA
